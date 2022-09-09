aab9c9bb55883db026af697765b62cd613c4f9ed
anavrinindonesia <112945020+anavrinindonesia@users.noreply.github.com>
2022-09-09 15:15:02 +0000

    Deploying to gh-pages from @ anavrinindonesia/anavrin@b506fbadd6a090327247e7040817237104235d68 🚀

--- _next/static/0sirr-BPZ_gPPnmWi-yRJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/751-0b79f5d16e49d056.js","static/chunks/pages/index-f2c3d3d9cc1d3796.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
